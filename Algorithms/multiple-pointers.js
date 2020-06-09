d9182d054bf680f73d2507029923b91fb406b25b
khaled-codeur <manou-codeur@outlook.com>
2020-06-09 15:17:35 +0100

    done in On2

--- Algorithms/multiple-pointers.js
+++ Algorithms/multiple-pointers.js
@@ -1,1 +1,15 @@
-//test
+function test (arr1, arr2, x) {
+    let lowset = Math.abs(arr1[0] + arr2[0] - x);
+    let pair = [];
+
+    for (let els1 of arr1) {
+        for (let els2 of arr2) {
+            if (Math.abs(els1 + els2 - x) < lowset) {
+                lowset = Math.abs(els1 + els2 - x);
+                pair = [els1, els2];
+            }
+        }
+    }
+    console.log(pair)
+}
+test([1,4,7,9], [10,20,30,40], 20);
