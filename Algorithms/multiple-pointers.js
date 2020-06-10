3e5a005519d0e8ae61af4efc944576bd0c126451
khaled-codeur <manou-codeur@outlook.com>
2020-06-10 13:01:30 +0100

    added another exapmle

--- Algorithms/multiple-pointers.js
+++ Algorithms/multiple-pointers.js
@@ -20,4 +20,31 @@ function test (arr1, arr2, x) {
     innerTest();
     console.log(pair);
 }
-test([1,4,7,9], [10,20,30,40], 20);
+// test([1,4,7,9], [10,20,30,40], 20);
+
+function findTriple (arr) {
+    let triple = [];
+    let i = arr.length-1;
+    let k = i-1;
+    let j = 0;
+    
+    while (i > 0) {
+        if (arr[j] + arr[k] === arr[i]) {
+            triple.push(arr[j], arr[k], arr[i]);
+            break;
+        }
+        if (arr[j] + arr[k] < arr[i]) {
+            j ++;
+        }else {
+            k--;        
+        }
+        if (j >= k) {
+            j = 0;
+            i --;
+            k = i-1;
+        }
+    }
+    if (triple.length > 0) console.log(triple);
+    else console.log(undefined);
+}
+findTriple([1,2,3,4,5]);
