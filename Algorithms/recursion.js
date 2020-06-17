f8c1c893c02899b7cf6a72a6b70edb9d108a7777
khaled-codeur <manou-codeur@outlook.com>
2020-06-17 21:37:43 +0100

    isPalindrom funct added

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -25,9 +25,7 @@ function reverse(str){
 function isPalindrome (str) {
     if (str.length === 1) return true;
     if (str[0] !== str[str.length-1]) return false;
-    
-    isPalindrome(str.slice(1, str.length-1));
-    
+    return isPalindrome(str.slice(1, str.length-1));   
 }
 
-console.log(isPalindrome('racedcar'));
+console.log(isPalindrome('racecar'));
