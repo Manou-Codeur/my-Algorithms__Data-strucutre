3e75798df9693446a1682402b4882066f4a8d828
khaled-codeur <manou-codeur@outlook.com>
2020-08-31 17:26:08 +0100

    worked around binary heap and implemented priority queue with it

---
+++ Data-Structure/binary-heap.js
@@ -0,0 +1,136 @@
+//heaps are similar to binary search tree, the only deference is that
+//binary heap can be maxHeap(always the parent node is greater than its child)
+//or minHeap(always the parent node is smaller than its child), not like BST where
+//the parent node has two child which one is greater than it's parent and the
+//other is smaller
+//the heap is emplemented using arrays, so there's no nodes with left and right..
+//so the idea is we use maths;
+//left = elementIndex*2 + 1
+//right = elementIndex*2 + 2
+//heap are most used when working with priority queue, and also for sorting
+//heap insertion and deletion time's complexity is O(log n) but for search is O(n)
+
+/*
+
+    --maxHeap example--
+
+            10
+        5        4
+     1     3   2    0   
+
+*/
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  //there is also the delete method
+}
+
+// let heap = new MaxBinaryHeap();
+// heap.insert(41);
+// heap.insert(20);
+// heap.insert(1);
+// heap.insert(4);
+// heap.insert(80);
+
+//using binary heap to implement priority queue
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+let ER = new PriorityQueue();
+ER.enqueue("common cold", 5);
+ER.enqueue("gunshot wound", 1);
+ER.enqueue("high fever", 4);
+ER.enqueue("broken arm", 2);
+ER.enqueue("glass in foot", 3);
